cc10016c95d3907855bf8cbcfc167dbdf6cfa22d
sprihozhiy <sprihozhiy@gmail.com>
2021-05-05 22:03:01 -0600

    client side initialized

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,10 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB is connected");
   } catch (err) {
     console.error(err.message);
